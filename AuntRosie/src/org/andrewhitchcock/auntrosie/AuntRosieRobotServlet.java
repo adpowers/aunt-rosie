89933fadc66a6bea72e7c3df89020e2264c7f65b
Andrew Hitchcock <mail@andrewhitchcock.org>
2009-10-09 23:33:36 -0700

    Extract the language target.

--- AuntRosie/src/org/andrewhitchcock/auntrosie/AuntRosieRobotServlet.java
+++ AuntRosie/src/org/andrewhitchcock/auntrosie/AuntRosieRobotServlet.java
@@ -7,6 +7,7 @@ import com.google.wave.api.Annotation;
 import com.google.wave.api.Blip;
 import com.google.wave.api.Event;
 import com.google.wave.api.EventType;
+import com.google.wave.api.Range;
 import com.google.wave.api.RobotMessageBundle;
 import com.google.wave.api.TextView;
 
@@ -25,10 +26,21 @@ public class AuntRosieRobotServlet extends AbstractRobot {
 		  if (e.getType() == EventType.DOCUMENT_CHANGED) {
 		    Blip blip = e.getBlip();
 		    
-		    // Skip waves I created.
+		    // Skip blips I created.
 		    if (blip.getContributors().contains("aunt-rosie@appspot.com")) {
 		      continue;
 		    }
+		    
+		    TextView doc = blip.getDocument();
+		    
+		    String keyword = "/translate:";
+		    int location = doc.getText().indexOf(keyword);
+		    if (location == -1) {
+		      continue; // nothing to translate
+		    }
+		    
+		    location += keyword.length();
+		    String languageTarget = doc.getText(new Range(location, location + 2));
 		    
 		    Blip myBlip;
 		    List<Blip> blips = blip.getChildren();
@@ -41,10 +53,11 @@ public class AuntRosieRobotServlet extends AbstractRobot {
 		    TextView myDoc = myBlip.getDocument();
 		    myDoc.delete();
 		    
-		    TextView doc = blip.getDocument();
 		    for (Annotation a : doc.getAnnotations()) {
 		      myDoc.append(a.toString() + "\n");
 		    }
+		    myDoc.append("\n");
+		    myDoc.append("languageTarget: " + languageTarget);
 		    myDoc.append("\n");
 		    myDoc.append(doc.getText());
 		  }
