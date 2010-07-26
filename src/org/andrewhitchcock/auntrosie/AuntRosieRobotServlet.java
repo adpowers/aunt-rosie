9408b818b83f5a4af7c108aad816764a89892bf9
Andrew <mail@andrewhitchcock.org>
2010-07-25 17:25:53 -0700

    Update to V2 of Google Wave API

--- src/org/andrewhitchcock/auntrosie/AuntRosieRobotServlet.java
+++ src/org/andrewhitchcock/auntrosie/AuntRosieRobotServlet.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.json.JSONObject;
 import org.json.JSONTokener;
@@ -14,14 +15,17 @@ import org.json.JSONTokener;
 import com.google.wave.api.AbstractRobot;
 import com.google.wave.api.Annotation;
 import com.google.wave.api.Blip;
-import com.google.wave.api.Event;
-import com.google.wave.api.EventType;
+import com.google.wave.api.BlipContentRefs;
+import com.google.wave.api.Element;
+import com.google.wave.api.ElementType;
 import com.google.wave.api.Gadget;
 import com.google.wave.api.Range;
-import com.google.wave.api.RobotMessageBundle;
-import com.google.wave.api.TextView;
+import com.google.wave.api.Restriction;
+import com.google.wave.api.event.DocumentChangedEvent;
+import com.google.wave.api.event.OperationErrorEvent;
 
 public class AuntRosieRobotServlet extends AbstractRobot {
+  private static final Logger log = Logger.getLogger(AuntRosieRobotServlet.class.getName());
 
 	private static final long serialVersionUID = -1546080376029476133L;
 
@@ -29,82 +33,100 @@ public class AuntRosieRobotServlet extends AbstractRobot {
 	private static final String buttonUrl = "http://aunt-rosie.appspot.com/translate-button.xml";
 	
 	@Override
-	public String getRobotName() {
+	protected String getRobotName() {
 		return "Aunt Rosie";
 	}
-
+	
 	@Override
-	public void processEvents(RobotMessageBundle bundle) {
-		for (Event e : bundle.getEvents()) {
-		  if (e.getType() == EventType.DOCUMENT_CHANGED) {
-		    Blip blip = e.getBlip();
-	      
-        // Skip blips I created.
-        if (blip.getCreator().equals(myAddress)) {
-          continue;
-        }
-        
-        TextView doc = blip.getDocument();
-        
-        if (doc.getAnnotations("lang").isEmpty()) {
-          continue;
-        }
-        
-        Gadget button = doc.getGadgetView().getGadget(buttonUrl);
-        if (button == null) {
-          doc.insert(0, "\n");
-          doc.insertElement(0, new Gadget(buttonUrl));
-          doc.insert(0, "\n");
-          continue;
-        }
-        
-        String languageTarget = button.getField("lang");
-        if (languageTarget == null || languageTarget.equals("none")) {
-          continue;
-        }
-        
-        List<Annotation> annotationsToTranslate = new ArrayList<Annotation>();
-        for (Annotation langAnnotations : doc.getAnnotations("lang")) {
-          // Don't try to translate a language to itself
-          if (! (langAnnotations.getValue().equals(languageTarget)
-              || langAnnotations.getValue().equals("unknown"))) {
-            annotationsToTranslate.add(langAnnotations);
-          }
-        }
-
-        Blip myBlip = null;
-        for (Blip child : blip.getChildren()) {
-          if (child.getCreator().equals(myAddress)) {
-            myBlip = child;
-            break;
-          }
-        }
-        if (myBlip == null) {
-          myBlip = blip.createChild();
-        }
-        
-        TextView myDoc = myBlip.getDocument();
-        myDoc.delete();
-        /*
-        myDoc.append(languageTarget + "\n");
-        myDoc.append(doc.getAnnotations().size() + "\n");
-        for (Annotation a : doc.getAnnotations()) {
-          myDoc.append(a + "\n");
-        }
-        myDoc.append(doc.getText() + "\n");
-        */
-        
-        for (Annotation a : annotationsToTranslate) {
-          Range r = a.getRange();
-          String translation = translateText(
-              a.getValue(),
-              languageTarget,
-              doc.getText(new Range(Math.max(0, r.getStart()), r.getEnd())));
-          myDoc.append(translation);
-        }
-        myDoc.appendMarkup("<br /><br /><br /><i>Translations powered by <a href=\"http://translate.google.com/#\">Google Translate</a></i>");
+	protected String getRobotAvatarUrl() {
+	  return "http://aunt-rosie.appspot.com/avatar.png";
+	}
+	
+	@Override
+	protected String getRobotProfilePageUrl() {
+    return "http://andrewhitchcock.org/?post=322";
+  }
+	
+	@Override
+  public void onDocumentChanged(DocumentChangedEvent event) {
+    Blip blip = event.getBlip();
+    
+    log.info("Received request");
+    
+    if (blip == null) {
+      return;
+    }
+    
+    log.info("Blip ID: " + blip.getBlipId());
+    
+    // Skip blips I created.
+    if (myAddress.equals(blip.getCreator())) {
+      return;
+    }
+    
+    if (blip.getAnnotations() == null
+        || blip.getAnnotations().get("lang") == null
+        || blip.getAnnotations().get("lang").isEmpty()) {
+      log.info("No lang annotations, skipping");
+      return;
+    }
+    
+    // Gadget.restrictByUrl
+    BlipContentRefs buttonRef = blip.first(ElementType.GADGET, Restriction.of("url", buttonUrl));
+    
+    if (buttonRef.value() == null) {
+      blip.at(1).insert("\n");
+      blip.at(1).insert(new Gadget(buttonUrl));
+      log.info("Inserted new gadget.");
+      return;
+    }
+    
+    Element buttonElement = buttonRef.value().asElement();
+    String languageTarget = buttonElement.getProperty("lang");
+    if (languageTarget == null || languageTarget.equals("none")) {
+      log.info("No language target selected");
+      return;
+    }
+    
+    List<Annotation> annotationsToTranslate = new ArrayList<Annotation>();
+    for (Annotation langAnnotations : blip.getAnnotations().get("lang")) {
+      // Don't try to translate a language to itself
+      if (! (langAnnotations.getValue().equals(languageTarget)
+          || langAnnotations.getValue().equals("unknown"))) {
+        annotationsToTranslate.add(langAnnotations);
       }
     }
+
+    Blip myBlip = null;
+    for (Blip child : blip.getChildBlips()) {
+      if (child.getCreator().equals(myAddress)) {
+        myBlip = child;
+        break;
+      }
+    }
+    if (myBlip == null) {
+      myBlip = blip.reply();
+    }
+    
+    myBlip.all().delete();
+    /*
+    myDoc.append(languageTarget + "\n");
+    myDoc.append(doc.getAnnotations().size() + "\n");
+    for (Annotation a : doc.getAnnotations()) {
+      myDoc.append(a + "\n");
+    }
+    myDoc.append(doc.getText() + "\n");
+    */
+    
+    for (Annotation a : annotationsToTranslate) {
+      Range r = a.getRange();
+      String translation = translateText(
+          a.getValue(),
+          languageTarget,
+          blip.range(Math.max(0, r.getStart()), r.getEnd()).value().getText());
+      myBlip.append(translation);
+    }
+    myBlip.appendMarkup("<br /><br /><br /><i>Translations powered by <a href=\"http://translate.google.com/#\">Google Translate</a></i>");
   }
   
   private String translateText(String from, String to, String text) {
@@ -130,11 +152,10 @@ public class AuntRosieRobotServlet extends AbstractRobot {
       return e.toString();
     }
   }
-
+  
   @Override
-  public void registerForEvents() {
-    // TODO Auto-generated method stub
-
+  public void onOperationError(OperationErrorEvent event) {
+    log.info(event.toString());
   }
 
 }
